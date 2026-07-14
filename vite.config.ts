0d51b8dd16f3c5ced924dd4b6982f0804014d8d5
msadmanarian <msadmanarian@gmail.com>
2026-07-15 00:45:29 +0600

    Resolved merge conflict in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
